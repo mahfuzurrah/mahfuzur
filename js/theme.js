3d3ded00a48e46819f8292d3cc8bfdc58bbc2f7e
Mahfuzur Rahman <mahfuzuruiux@gmail.com>
2022-11-09 14:41:00 +0600

    aa

--- js/theme.js
+++ js/theme.js
@@ -289,3 +289,18 @@
 })(jQuery);
 
 
+// contttttttttttttttttttttttttttttttttttttttt
+
+Email.send({
+  Host : "smtp.elasticemail.com",
+  Username : "username",
+  Password : "password",
+  To : 'them@website.com',
+  From : "you@isp.com",
+  Subject : "This is the subject",
+  Body : "And this is the body"
+}).then(
+message => alert(message)
+);
+
+
--- vendor/mail.php
+++ vendor/mail.php
@@ -1,12 +1,17 @@
 <?php
+//get data from form  
+
 $name = $_POST['name'];
-$email = $_POST['email'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
-$formcontent=" From: $name \n subject: $subject \n  Message: $message";
-$recipient = "mahfuzuruiux@gmail.com";
-$subject = "Add Your Subject Here";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-header("location: thank-you.html")
+$email= $_POST['email'];
+$message= $_POST['message'];
+$to = "mahfuzuruiux@gmail.com";
+$subject = "Mail From website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: noreply@yoursite.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:thankyou.html");
 ?>
