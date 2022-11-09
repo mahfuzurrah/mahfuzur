2f3082d5089e4434804a2bcf1e546ba357b0d492
Mahfuzur Rahman <mahfuzuruiux@gmail.com>
2022-11-09 14:41:05 +0600

    aa

--- js/theme.js
+++ js/theme.js
@@ -291,16 +291,16 @@
 
 // contttttttttttttttttttttttttttttttttttttttt
 
-Email.send({
-  Host : "smtp.elasticemail.com",
-  Username : "username",
-  Password : "password",
-  To : 'them@website.com',
-  From : "you@isp.com",
-  Subject : "This is the subject",
-  Body : "And this is the body"
-}).then(
-message => alert(message)
-);
+// Email.send({
+//   Host : "smtp.elasticemail.com",
+//   Username : "username",
+//   Password : "password",
+//   To : 'them@website.com',
+//   From : "you@isp.com",
+//   Subject : "This is the subject",
+//   Body : "And this is the body"
+// }).then(
+// message => alert(message)
+// );
 
 
